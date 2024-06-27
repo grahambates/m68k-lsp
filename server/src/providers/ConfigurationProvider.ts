6d6994814819d1f2b1f9b25cbdd6e23863401f40
Graham Bates <info@grahambates.com>
2024-06-28 00:50:33 +0100

    Workspace config files

--- server/src/providers/ConfigurationProvider.ts
+++ server/src/providers/ConfigurationProvider.ts
@@ -1,18 +1,55 @@
 import * as lsp from "vscode-languageserver";
 import { DidChangeConfigurationNotification } from "vscode-languageserver";
+import { URI } from "vscode-uri";
 import { Provider } from ".";
-import { mergeConfig } from "../config";
+import { Config, mergeConfig } from "../config";
 import { Context } from "../context";
+import { readdirSync, readFileSync, watch } from "fs";
 
 export default class ConfiguratonProvider implements Provider {
-  constructor(protected readonly ctx: Context) {}
+  protected clientConfig: Config;
+
+  constructor(protected readonly ctx: Context) {
+    this.clientConfig = ctx.config;
+    this.updateConfig();
+  }
+
+  updateConfig() {
+    const workspaceConfig = this.findWorkspaceConfig();
+    this.ctx.config = workspaceConfig
+      ? mergeConfig(workspaceConfig, this.clientConfig)
+      : this.clientConfig;
+  }
+
+  findWorkspaceConfig(): Partial<Config> | null {
+    for (const { uri } of this.ctx.workspaceFolders) {
+      const path = URI.parse(uri).fsPath;
+      const contents = readdirSync(path);
+      const foundPath = contents.find((file) => file.match(/\.m68krc/i));
+
+      if (foundPath) {
+        this.ctx.logger.info("Found workspace config " + foundPath);
+        try {
+          const configJson = readFileSync(foundPath).toString();
+          return JSON.parse(configJson);
+        } catch (err) {
+          if (err instanceof Error) {
+            this.ctx.logger.error("Error loading config: " + err.message);
+          }
+        }
+      }
+    }
+    this.ctx.logger.info("No workspace config found");
+    return null;
+  }
 
   async onDidChangeConfiguration() {
-    const oldConfig = this.ctx.config;
+    const oldConfig = this.clientConfig;
     const newConfig = await this.ctx.connection.workspace.getConfiguration(
       "m68k"
     );
-    this.ctx.config = mergeConfig(newConfig, oldConfig);
+    this.clientConfig = mergeConfig(newConfig, oldConfig);
+    this.updateConfig();
   }
 
   register(connection: lsp.Connection, capabilities: lsp.ClientCapabilities) {
@@ -29,6 +66,19 @@ export default class ConfiguratonProvider implements Provider {
         );
       });
     }
+
+    // Watch for .m68krc file changes (debounced)
+    let updateConfigTimeout: NodeJS.Timeout;
+    for (const { uri } of this.ctx.workspaceFolders) {
+      const path = URI.parse(uri).fsPath;
+      watch(path, (_, filename) => {
+        if (filename.match(/\.m68krc/i)) {
+          clearTimeout(updateConfigTimeout);
+          updateConfigTimeout = setTimeout(this.updateConfig.bind(this), 500);
+        }
+      });
+    }
+
     return {};
   }
 }
